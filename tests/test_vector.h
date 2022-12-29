b0de57fceb33cc8346d74d4e63a361d3cc3929e2
zhihao <1098610047@qq.com>
2022-12-29 19:53:17 +0800

    vector test (now blank)

---
+++ tests/test_vector.h
@@ -0,0 +1,14 @@
+//
+// Created by 镜瞳 on 2022/12/29.
+//
+
+#ifndef TINYTINYSTL_TEST_VECTOR_H
+#define TINYTINYSTL_TEST_VECTOR_H
+#include "../vector.h"
+namespace tinystl::test::vector_test {
+    void test_vector() {
+
+    }
+
+}
+#endif //TINYTINYSTL_TEST_VECTOR_H
