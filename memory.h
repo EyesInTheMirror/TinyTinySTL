706bfff30b754afeb199d6f3bf04e6e4a7071932
zhihao <1098610047@qq.com>
2022-12-29 19:56:59 +0800

    memory-related operations

---
+++ memory.h
@@ -0,0 +1,13 @@
+//
+// Created by 镜瞳 on 2022/12/29.
+//
+
+#ifndef TINYTINYSTL_MEMORY_H
+#define TINYTINYSTL_MEMORY_H
+//这个头文件包含所有和内存分配、对象构造相关的操作
+#include "uninitialized.h"
+#include "allocator.h"
+#include "construct.h"
+
+
+#endif //TINYTINYSTL_MEMORY_H
