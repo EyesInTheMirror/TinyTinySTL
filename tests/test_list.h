2416f536249d31f9719e3f1426a4301acfda909f
zhihao <1098610047@qq.com>
2023-01-14 09:54:18 +0800

    update

--- tests/test_list.h
+++ tests/test_list.h
@@ -16,27 +16,51 @@ namespace tinystl::test::list_test {
     }
 
     void test_list() {
-
+        using std::cout;
+        using std::endl;
+        //构造
         int iv[5] = {1, 2, 3, 4, 5};
         list<int> l(iv, iv + 5);
         list<int> l2(3, 99);
+        print_list(l);
+        print_list(l2);
+        std::cout << "------------------" << std::endl;
+
+        //元素存取
+        cout << "l.front() = " << l.front() << endl;
+        cout << "l.back() = " << l.back() << endl;
+        cout << "l.size() = " << l.size() << endl;
         *(l2.begin()) = 10;
         print_list(l2);
-        auto itr = std::find(l2.begin(), l2.end(), 99);
+        std::cout << "------------------" << std::endl;
 
-        l2.splice(itr, l);
+        //插入
+        l.push_back(100);
+        l.push_front(100);
+        auto itr = std::find(l.begin(), l.end(), 3);
+        l.insert(itr, 100);
+        print_list(l);
+        std::cout << "------------------" << std::endl;
+
+        //删除
+        l.erase(l.begin());
+        print_list(l);
+        l.push_back(100);
+        l.push_back(100);
+        print_list(l);
+        l.unique();
+        print_list(l);
+        l.remove(100);
+        print_list(l);
+        std::cout << "------------------" << std::endl;
+
+        //骚操作
+        auto itr2 = std::find(l2.begin(), l2.end(), 99);
+        l2.splice(itr2, l);
+        if(l.empty()) cout << "l is now empty!" << endl;
         print_list(l2);
         l2.reverse();
         print_list(l2);
-        std::cout << "------------------" << std::endl;
-        l2.swap(l);
-        print_list(l);
-        print_list(l2);
-        l2.swap(l);
-        print_list(l);
-        print_list(l2);
-        std::cout << "------------------" << std::endl;
-        l2.pop_front();
         l2.sort();
         print_list(l2);
     }
